0f48d006472bb572b8a2fc1d38c6e3db812f1aae
Gordon <gordon@codeplay.com>
2015-01-23 23:05:34 +0000

    add inital assets

---
+++ Alcove/Assets/team_controller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class team_controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
