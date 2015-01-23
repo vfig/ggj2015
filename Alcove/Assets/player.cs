626aeef36a164e560f91d1aa84984fe3d5c1b0ea
Gordon <gordon@codeplay.com>
2015-01-23 23:23:29 +0000

    rename the first set of scripts

--- Alcove/Assets/player.cs
+++ Alcove/Assets/player.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class team_controller : MonoBehaviour {
+public class player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
