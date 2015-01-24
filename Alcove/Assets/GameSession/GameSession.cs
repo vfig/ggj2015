9990935c17743ba4e275c75c74c0f7f91d2ec772
Cloov <industrion@gmail.com>
2015-01-24 00:39:41 +0000

    More gamestate stuff, yo

--- Alcove/Assets/GameSession/GameSession.cs
+++ Alcove/Assets/GameSession/GameSession.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class GameSession : MonoBehaviour {
 
+	const bool SHOW_DEBUG_TEXT = true;
+
 	public enum GameplayState {
 		Pregame,
 		InProgress,
@@ -11,19 +13,63 @@ public class GameSession : MonoBehaviour {
 
 	GameplayState gameplayState;
 
+	// On 'IPlayer' etc.
+	// These interface types are just put in place until we have more vision of each other's code and are ready to integrate more.
 	Object player1;
 	Object player2;
 
 	void Start() {
-		SetGameplayState(GameplayState.Pregame);
+		//player1.GetTower().AddComponent("")
+		StartNewGame();
 	}
 
 	void Update() {
+	}
 
+	void OnGUI() {
+		if(SHOW_DEBUG_TEXT) {
+			GUI.Label(new Rect(10, 10, 200, 30), "Gameplay state: " + gameplayState);
+		}
 	}
 
 	void SetGameplayState(GameplayState state) {
+
 		Debug.Log("Setting gameplay state to: " + state);
 		gameplayState = state;
+
+		switch(state) {
+		case GameplayState.Pregame:
+			SetupPregameState();
+			break;
+		case GameplayState.InProgress:
+			SetupInProgressState();
+			break;
+		case GameplayState.Roundup:
+			SetupRoundupState();
+			break;
+		}
 	}
+
+	void StartNewGame() {
+		Debug.Log("Starting new game.");
+
+		//player1.Reset();
+		//player2.Reset();
+
+		SetGameplayState(GameplayState.Pregame);
+	}
+
+	void SetupPregameState() {
+		Debug.Log("Setting up Pregame state.");
+	}
+
+	void SetupInProgressState() {
+		Debug.Log("Setting up InProgress state.");
+	}
+
+	void SetupRoundupState() {
+		Debug.Log("Setting up Roundup state.");
+	}
+
+
 }
