189618fccff58535cb9611e331354e1d3cad17a4
Gordon <gordon@codeplay.com>
2015-01-24 12:51:17 +0000

    Added EmptyTowerSegment script.

---
+++ Alcove/Assets/Tower/EmptyTowerSegment.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmptyTowerSegment : TowerSegment {
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	
+	public virtual bool IsActionable () {
+		return true;
+	}
+	
+	public virtual void PerformAction () {
+		
+		/* TODO: Select New Segment Prefab  */
+		
+		
+	}
+}
