b59ee8e26968396692cc5f896efa8f767e84cf06
Andy Durdin <me@andy.durdin.net>
2015-01-24 14:27:13 +0000

    Pretty tribe UI is pretty.

---
+++ Alcove/Assets/Player/TribeUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TribeUI : MonoBehaviour {
+	public Tribe tribe;
+	private CanvasGroup group;
+	private Image image;
+	private Text text;
+
+	public void Start() {
+		group = GetComponentInChildren<CanvasGroup>();
+		image = GetComponentInChildren<Image>();
+		text = GetComponentInChildren<Text>();
+	}
+
+	public void Update() {
+		text.text = tribe.count.ToString();
+		if (tribe.IsBusy) {
+			group.alpha = 0.25f + (0.5f * tribe.BusyFraction);
+			image.fillAmount = tribe.BusyFraction;
+		} else {
+			group.alpha = 1.0f;
+			image.fillAmount = 1.0f;
+		}
+	}
+}
