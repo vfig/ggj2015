ca3c506501432dc3c6c40c8877f7417da9d67b50
Cloov <industrion@gmail.com>
2015-01-24 13:26:18 +0000

    Added end scene for populating later

---
+++ Alcove/Assets/EndScene/EndScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScene : MonoBehaviour {
+
+	void Start() {
+	
+	}
+
+	void Update() {
+		if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) {
+			Application.LoadLevel("StartScene");
+		}
+	}
+}
