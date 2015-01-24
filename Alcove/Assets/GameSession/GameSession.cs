7c11617378388ae56e16e7eac8652073672b4044
Cloov <industrion@gmail.com>
2015-01-24 00:13:34 +0000

    Added a very very empty GameSession starting point

---
+++ Alcove/Assets/GameSession/GameSession.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSession : MonoBehaviour {
+
+	public enum GameplayState {
+		Pregame,
+		InProgress,
+		Roundup
+	}
+
+	GameplayState gameplayState;
+
+	Object player1;
+	Object player2;
+
+	void Start() {
+		SetGameplayState(GameplayState.Pregame);
+	}
+
+	void Update() {
+
+	}
+
+	void SetGameplayState(GameplayState state) {
+		Debug.Log("Setting gameplay state to: " + state);
+		gameplayState = state;
+	}
+}
