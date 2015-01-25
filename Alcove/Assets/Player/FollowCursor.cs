887efa6bcfaae0dd052004ae87f7b458a041c189
Andy Durdin <me@andy.durdin.net>
2015-01-25 06:46:08 +0000

    Fix camera flicker.

--- Alcove/Assets/Player/FollowCursor.cs
+++ Alcove/Assets/Player/FollowCursor.cs
@@ -12,7 +12,7 @@ public class FollowCursor : MonoBehaviour {
 		cameraTransform = camera.transform;
 	}
 	
-	public void Update () {
+	public void LateUpdate () {
 		Vector3 cameraPosition = cameraTransform.position;
 		Vector3 cursorPosition = cursorTransform.position;
 
@@ -27,12 +27,12 @@ public class FollowCursor : MonoBehaviour {
 		float minY = camera.ViewportToWorldPoint(cursorBottom).y;
 		float maxY = camera.ViewportToWorldPoint(cursorTop).y;
 
-		minY = Mathf.Max(minY, 3.5f);
-
 		// Lerp the camera to the desired position if possible, yet clamp to ensure it is always on screen.
 		Vector3 idealCameraPosition = new Vector3(cursorPosition.x, cursorPosition.y, cameraPosition.z);
 		Vector3 lerpedCameraPosition = Vector3.Lerp(cameraPosition, idealCameraPosition, Time.deltaTime);
 		lerpedCameraPosition.y = Mathf.Clamp(lerpedCameraPosition.y, minY, maxY);
+		// Force the camera to never go too low
+		lerpedCameraPosition.y = Mathf.Max(lerpedCameraPosition.y, 3.5f);
 		camera.transform.position = lerpedCameraPosition;
 	}
 }
