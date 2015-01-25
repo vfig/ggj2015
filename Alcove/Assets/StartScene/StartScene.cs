846dc7d09568b48395cf76bddba50c95c0cc37e3
Cloov <industrion@gmail.com>
2015-01-25 13:53:49 +0000

    Changed text display on Start scene

--- Alcove/Assets/StartScene/StartScene.cs
+++ Alcove/Assets/StartScene/StartScene.cs
@@ -11,9 +11,9 @@ public class StartScene : MonoBehaviour {
 		GameInput.Update();
 
 		if(GameInput.GetAnyStartButtonDown()) {
-			Application.LoadLevel("GameScene");
-		} else if(GameInput.GetAnyTribeButtonDown(0) || GameInput.GetAnyTribeButtonDown(1)) {
 			Application.LoadLevel("InstructionsScene");
+		} else if(GameInput.GetAnyTribeButtonDown(0) || GameInput.GetAnyTribeButtonDown(1)) {
+			Application.LoadLevel("GameScene");
 		} else if(GameInput.GetAnyCancelButtonDown()) {
 			Application.LoadLevel("EndScene");
 		}
