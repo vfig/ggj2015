312631a3da4abe0aef69933cbadd8d6763a3ffc3
Andy Durdin <me@andy.durdin.net>
2015-01-25 13:17:48 +0000

    Show worker costs before building.

--- Alcove/Assets/Tower/PrefabSelector.cs
+++ Alcove/Assets/Tower/PrefabSelector.cs
@@ -1,38 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class PrefabSelector : MonoBehaviour {
-	private List<Transform> m_selections;
+	private int m_selectedIndex;
+	private Transform m_selectedSprite;
+	private List<TowerSegment> m_towerSegments;
 	private const float spriteSpacing = 5;
 
+	public Text buildingCost;
+
 	void Awake () {
-		m_selections = new List<Transform>();
+		m_towerSegments = new List<TowerSegment>();
 	}
 
 	public void Start() {
-	}
-
-	public void AddSelection(Sprite sprite) {
 		GameObject obj = new GameObject();
 		SpriteRenderer spriteRenderer = obj.AddComponent<SpriteRenderer>();
-		spriteRenderer.sprite = sprite;
-		spriteRenderer.enabled = false;
 		spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f, 0.75f);
 		spriteRenderer.sortingOrder = 4;
 		obj.transform.localScale = 0.75f * Vector3.one;
 		obj.transform.parent = transform;
 		obj.transform.localPosition = Vector3.zero;
-		m_selections.Add(obj.transform);
+		m_selectedSprite = obj.transform;
+
+		// Not using this label for now.
+		buildingCost.gameObject.SetActive(false);
+	}
+
+	public void AddSelection(TowerSegment segment) {
+		m_towerSegments.Add(segment);
 	}
 
 	public void SetSelectedIndex(int index) {
-		for (int i = 0; i < m_selections.Count; ++i) {
-			SpriteRenderer spriteRenderer = m_selections[i].gameObject.GetComponent<SpriteRenderer>();
-			spriteRenderer.enabled = (i == index);
-		}
+		m_selectedIndex = index;
+		TowerSegment segment = m_towerSegments[m_selectedIndex];
+		Sprite sprite = segment.GetComponent<SpriteRenderer>().sprite;
+		m_selectedSprite.GetComponent<SpriteRenderer>().sprite = sprite;
 	}
 
 	public void Update () {
+		TowerSegment segment = m_towerSegments[m_selectedIndex];
+		buildingCost.text = segment.OnGetTribeCost().ToString() + " workers";
 	}
 }
